5201dda8b7a7fcefa97180aec443bc7a1ad5747c
DaniFdezCab <danfercab@alum.us.es>
2024-06-26 10:13:40 +0200

    Funcionamiento perfecto rondas implementadas bugs fixed

--- frontend/src/game/Game.js
+++ frontend/src/game/Game.js
@@ -33,11 +33,12 @@ const WebSocketComponent = () => {
     const [cardPlayed1, setCardPlayed1] = useState(null);
 
 
-    const [waiting, setWaiting] = useState(false);
+    const [waiting, setWaiting] = useState(false); //Estado para saber si el otro jugador ya ha jugado
+    const [played, setPlayed] = useState(false); //Estado para bloquear los botones una vez jugada una carta
 
 
     //Cosas en comun
-    const [deckOfCards, setDeckOfCards] = useState(generateUniqueRandomNumbers());
+    const [deckOfCards, setDeckOfCards] = useState(generateUniqueRandomNumbers());// Crea un array de números del 1 al 50);
     const [cardsDiscard, setCardsDiscard] = useState([]);
     const [stompClient, setStompClient] = useState(null);
 
@@ -65,7 +66,6 @@ const WebSocketComponent = () => {
             })
             .catch(error => { console.error('Error fetching match:', error); return null; });
 
-
     }
 
     useEffect(() => {
@@ -87,14 +87,18 @@ const WebSocketComponent = () => {
                     setCards0(body.player0Cards);
                 }
                 if (body.type === 'READY') {
-                    setReceived(true)
+                    setReceived(true);
                 }
                 if (body.type === 'PLAYEDCARD') {
                     if (playerNumber === 0 && body.playedCard1 !== -1) {
                         setCardPlayed1(body.playedCard1);
+                        setShowCards(false);
+                        setWaiting(true);
                     }
                     if (playerNumber === 1 && body.playedCard0 !== -1) {
                         setCardPlayed0(body.playedCard0);
+                        setShowCards(false);
+                        setWaiting(true);
                     }
                 }
 
@@ -108,24 +112,85 @@ const WebSocketComponent = () => {
             }
         };
 
-    }, [matchId, playerNumber, received]);
+    }, [matchId, playerNumber]);
 
 
     useEffect(() => {
-        if (stompClient && playerNumber === 0 && received) {
-            const cardsPlayer0 = initialDeal(deckOfCards);
-            setCards0(cardsPlayer0);
-            const cardsPlayer1 = initialDeal(deckOfCards)
-            setCards1(cardsPlayer1)
-            handleSendDeckMessage('DECKS');
+        let interval = null;
+        if (stompClient && playerNumber === 0 && received && deckOfCards.length === 50) {
+            console.log(deckOfCards);
+            const cards = initialDeal(deckOfCards);
+            setDeckOfCards(cards[0]);
+            setCards0(cards[1]);
+            setCards1(cards[2]);
         }
         if (playerNumber === 1 && !received) {
-            handleSendDeckMessage('RECEIVED');
+            interval = setInterval(() => {
+                handleSendDeckMessage('RECEIVED')
+            }, 1000);
+
         }
-
-
+        return () => {
+            clearInterval(interval);
+        };
     }, [playerNumber, stompClient, received]);
 
+
+    // Reparto inicial
+    useEffect(() => {
+        if (cards0.length > 0 && cards1.length > 0 && playerNumber === 0 && received) {
+            handleSendDeckMessage('DECKS');
+            setReceived(false);
+        }
+    }, [cards0, cards1]);
+
+
+    // Confirmaciones del turno y realización de acciones
+    useEffect(() => {
+        if (cardPlayed0 > 0 && cardPlayed1 > 0 && played) {
+            setShowCards(true);
+            // HACER ACCIONES DE CARTAS
+            setWaiting(false);
+            setPlayed(false);
+            setShowConfirmationModal(true);
+        }
+    }, [cardPlayed0, cardPlayed1, played]);
+
+
+
+    const handleActionConfirmed = () => {
+        setShowConfirmationModal(false);
+        setShowCards(false);
+        if (waiting) { //Se comprueba si ya ha recibido otra carta por pate del otro jugador, si es así entonces solo reseteamos la carta propia
+            switch (playerNumber) {
+                case 0:
+                    setCardPlayed0(-1);
+                    break;
+                case 1:
+                    setCardPlayed1(-1);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else {
+            setCardPlayed0(-1);
+            setCardPlayed1(-1);
+        }
+    }
+
+    async function handleSetCardPlayed(player, cardNumber) {
+        if (player === 0 && !played) {
+            await setCardPlayed0(cardNumber === 51 ? cardNumber : cards0[cardNumber]);
+            handleSendDeckMessage('PLAYEDCARD', cardNumber === 51 ? cardNumber : cards0[cardNumber]);
+            setPlayed(true);
+        }
+        if (player === 1 && !played) {
+            await setCardPlayed1(cardNumber === 51 ? cardNumber : cards1[cardNumber]);
+            handleSendDeckMessage('PLAYEDCARD', cardNumber === 51 ? cardNumber : cards1[cardNumber]);
+            setPlayed(true);
+        }
+    }
 
     async function handleSendDeckMessage(type, cardNumber = -1) {
         if (type === 'DECKS') {
@@ -155,38 +220,8 @@ const WebSocketComponent = () => {
         }
     }
 
-
-    useEffect(() => {
-        if (cardPlayed0 > 0 && cardPlayed1 > 0) {
-            setShowCards(true);
-            // HACER ACCIONES DE CARTAS
-            setShowConfirmationModal(true);
-        }
-    }, [cardPlayed0, cardPlayed1])
-
-    async function handleSetCardPlayed(player, cardNumber) {
-        if (player === 0 && !waiting) {
-            setWaiting(true);
-            await setCardPlayed0(cards0[cardNumber]);
-            handleSendDeckMessage('PLAYEDCARD', cards0[cardNumber]);
-        }
-        if (player === 1 && !waiting) {
-            setWaiting(true);
-            await setCardPlayed1(cards1[cardNumber]);
-            handleSendDeckMessage('PLAYEDCARD', cards1[cardNumber]);
-        }
-    }
-
     const handleMouseEnter = (imgSrc) => {
         setRightButtonImg(imgSrc)
-    }
-
-    const handleActionConfirmed = () => {
-        setShowConfirmationModal(false);
-        setShowCards(false);
-        setWaiting(false);
-        setCardPlayed0(-1);
-        setCardPlayed1(-1);
     }
 
     return (
@@ -227,6 +262,7 @@ const WebSocketComponent = () => {
                     <CardButton className="large-button" onClick={() => handleSetCardPlayed(0, 4)} imgSrc={`${process.env.PUBLIC_URL}/cards/card${cards0[4]}.png`} onMouseEnter={() => handleMouseEnter(`${process.env.PUBLIC_URL}/cards/card${cards0[4]}.png`)} />
                     <CardButton className="large-button" onClick={() => handleSetCardPlayed(0, 5)} imgSrc={`${process.env.PUBLIC_URL}/cards/card${cards0[5]}.png`} onMouseEnter={() => handleMouseEnter(`${process.env.PUBLIC_URL}/cards/card${cards0[5]}.png`)} />
                     <CardButton className="large-button" onClick={() => handleSetCardPlayed(0, 6)} imgSrc={`${process.env.PUBLIC_URL}/cards/card${cards0[6]}.png`} onMouseEnter={() => handleMouseEnter(`${process.env.PUBLIC_URL}/cards/card${cards0[6]}.png`)} />
+                    <CardButton className="large-button" onClick={() => handleSetCardPlayed(1, 51)} imgSrc={`${process.env.PUBLIC_URL}/cards/card51.png`} onMouseEnter={() => handleMouseEnter(`${process.env.PUBLIC_URL}/cards/card51.png`)} />
                 </div>
                 :
                 <div className="bottom-row">
@@ -237,6 +273,7 @@ const WebSocketComponent = () => {
                     <CardButton className="large-button" onClick={() => handleSetCardPlayed(1, 4)} imgSrc={`${process.env.PUBLIC_URL}/cards/card${cards1[4]}.png`} onMouseEnter={() => handleMouseEnter(`${process.env.PUBLIC_URL}/cards/card${cards1[4]}.png`)} />
                     <CardButton className="large-button" onClick={() => handleSetCardPlayed(1, 5)} imgSrc={`${process.env.PUBLIC_URL}/cards/card${cards1[5]}.png`} onMouseEnter={() => handleMouseEnter(`${process.env.PUBLIC_URL}/cards/card${cards1[5]}.png`)} />
                     <CardButton className="large-button" onClick={() => handleSetCardPlayed(1, 6)} imgSrc={`${process.env.PUBLIC_URL}/cards/card${cards1[6]}.png`} onMouseEnter={() => handleMouseEnter(`${process.env.PUBLIC_URL}/cards/card${cards1[6]}.png`)} />
+                    <CardButton className="large-button" onClick={() => handleSetCardPlayed(1, 51)} imgSrc={`${process.env.PUBLIC_URL}/cards/card51.png`} onMouseEnter={() => handleMouseEnter(`${process.env.PUBLIC_URL}/cards/card51.png`)} />
                 </div>
             }
 
--- frontend/src/util/game/utils.js
+++ frontend/src/util/game/utils.js
@@ -1,29 +1,25 @@
-export function generateUniqueRandomNumbers(count = 50, min = 1, max = 50) {
-    if (count > (max - min + 1)) {
-        throw new Error("Count exceeds the range of unique numbers available.");
-    }
-
-    const numbers = [];
-    while (numbers.length < count) {
-        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-        if (!numbers.includes(randomNum)) {
-            numbers.push(randomNum);
-        }
+export function generateUniqueRandomNumbers() {
+    const numbers = Array.from({ length: 50 }, (_, i) => i + 1); // Crea un array de números del 1 al 50
+    for (let i = numbers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // Selecciona un índice aleatorio
+        [numbers[i], numbers[j]] = [numbers[j], numbers[i]]; // Intercambia los elementos
     }
     return numbers;
 }
-
 export function initialDeal(deck) {
-    const newDeck = [];
+    const deckPlayer0 = [];
+    const deckPlayer1 = [];
     for (let i = 0; i < 7; i++) {
-        newDeck.push(deck.shift());
+        deckPlayer0.push(deck.shift());
       }
-    
-    return newDeck;
+    for (let i = 0; i < 7; i++) {
+        deckPlayer1.push(deck.shift());
+      }
+    return [deck, deckPlayer0, deckPlayer1];
 }
 
 export function steal(deck) {
     const myDeck= deck;
     myDeck.push(deck.shift());
-    return myDeck
+    return myDeck;
 }
