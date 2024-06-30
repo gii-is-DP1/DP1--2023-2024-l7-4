375ebcd0ff4fc4b6252f63728422b384c2ae5742
Oscar Menendez Marquez <oscmenmar@alum.us.es>
2024-06-30 17:37:02 +0200

    register terminado

--- frontend/src/auth/register/index.js
+++ frontend/src/auth/register/index.js
@@ -2,33 +2,17 @@ import "../../static/css/auth/authButton.css";
 import "../../static/css/auth/authPage.css";
 import tokenService from "../../services/token.service";
 import FormGenerator from "../../components/formGenerator/formGenerator";
-import { registerFormOwnerInputs } from "./form/registerFormOwnerInputs";
-import { registerFormVetInputs } from "./form/registerFormVetInputs";
-import { registerFormClinicOwnerInputs } from "./form/registerFormClinicOwnerInputs";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+import { registerFormPlayerInputs } from "./form/registerFormPlayerInputs";
 
 export default function Register() {
-  let [type, setType] = useState(null);
-  let [authority, setAuthority] = useState(null);
-  let [clinics, setClinics] = useState([]);
-
   const registerFormRef = useRef();
-
-  function handleButtonClick(event) {
-    const target = event.target;
-    let value = target.value;
-    if (value === "Back") value = null;
-    else setAuthority(value);
-    setType(value);
-  }
 
   function handleSubmit({ values }) {
 
     if(!registerFormRef.current.validate()) return;
 
     const request = values;
-    request.clinic = clinics.filter((clinic) => clinic.name === request.clinic)[0];
-    request["authority"] = authority;
     let state = "";
 
     fetch("/api/v1/auth/signup", {
@@ -62,7 +46,7 @@ export default function Register() {
               else {
                 tokenService.setUser(data);
                 tokenService.updateLocalAccessToken(data.token);
-                window.location.href = "/dashboard";
+                window.location.href = "/";
               }
             })
             .catch((message) => {
@@ -76,44 +60,16 @@ export default function Register() {
   }
 
   useEffect(() => {
-    if (type === "Owner" || type === "Vet") {
-      if (registerFormOwnerInputs[5].values.length === 1){
-        fetch("/api/v1/clinics")
-        .then(function (response) {
-          if (response.status === 200) {
-            return response.json();
-          } else {
-            return response.json();
-          }
-        })
-        .then(function (data) {
-          setClinics(data);
-          if (data.length !== 0) {
-            let clinicNames = data.map((clinic) => {
-              return clinic.name;
-            });
+      });
 
-            registerFormOwnerInputs[5].values = ["None", ...clinicNames];
-          }
-        })
-        .catch((message) => {
-          alert(message);
-        });
-      }
-    }
-  }, [type]);
-
-  if (type) {
-    return (
-      <div className="auth-page-container">
-        <h1>Register</h1>
-        <div className="auth-form-container">
-          <FormGenerator
+   return (
+    <div className="auth-page-container">
+      <h1>Register</h1>
+      <div className="auth-form-container">
+        <FormGenerator
             ref={registerFormRef}
             inputs={
-              type === "Owner" ? registerFormOwnerInputs 
-              : type === "Vet" ? registerFormVetInputs
-              : registerFormClinicOwnerInputs
+              registerFormPlayerInputs
             }
             onSubmit={handleSubmit}
             numberOfColumns={1}
@@ -124,39 +80,5 @@ export default function Register() {
         </div>
       </div>
     );
-  } else {
-    return (
-      <div className="auth-page-container">
-        <div className="auth-form-container">
-          <h1>Register</h1>
-          <h2 className="text-center text-md">
-            What type of user will you be?
-          </h2>
-          <div className="options-row">
-            <button
-              className="auth-button"
-              value="Owner"
-              onClick={handleButtonClick}
-            >
-              Owner
-            </button>
-            <button
-              className="auth-button"
-              value="Vet"
-              onClick={handleButtonClick}
-            >
-              Vet
-            </button>
-            <button
-              className="auth-button"
-              value="Clinic Owner"
-              onClick={handleButtonClick}
-            >
-              Clinic Owner
-            </button>
-          </div>
-        </div>
-      </div>
-    );
+  
   }
-}
